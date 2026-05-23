b7d5b9aa0e65ab15bf4def75e0c8ba43f3f78385
Mahdi Zargarzadeh <mahdizargarzadeh0@gmail.com>
2026-05-23 23:35:13 +0330

    Add bfs Algo

--- search/bfs.py
+++ search/bfs.py
@@ -1,2 +1,28 @@
-def bfs():
-    pass
+from collections import deque
+
+def bfs(start):
+
+    if start.is_goal_state():
+        return []
+
+    q = deque()
+    q.append((start, []))
+    visited = set()
+    visited.add(start)
+
+    while q:
+        state, path = q.popleft()
+
+        for action, cost, next_state in state.get_successors():
+
+            if next_state.is_collision_state():
+                continue
+
+            if next_state.is_goal_state():
+                return path + [action]
+
+            if next_state not in visited:
+                visited.add(next_state)
+                q.append((next_state, path + [action]))
+
+    return []
