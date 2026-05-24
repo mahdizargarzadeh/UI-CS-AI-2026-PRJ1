c7c92b56a689eb4f9fda831cd72be8313e7f4477
mnmgalisafa <mnmgalisafa@gmail.com>
2026-05-24 12:23:38 -0700

    Update ucs.py

--- search/ucs.py
+++ search/ucs.py
@@ -1,2 +1,36 @@
-def ucs():
-    pass
+import heapq
+
+def ucs(start):
+  if start.is_goal_state() == True:
+      return []
+  counter = 0
+  queue = []
+  heapq.heappush(queue, (0, counter, start, []))
+  costs = {}
+  costs[start] = 0
+  
+  while len(queue) > 0:
+      current = heapq.heappop(queue)
+      cost = current[0]
+      state = current[2]
+      path = current[3]
+      if cost > costs[state]:
+          continue
+      if state.is_goal_state() == True:
+          return path
+          
+      for action, stepcost, nextstate in state.get_successors():
+          if nextstate.is_collision_state() == True:
+              continue
+          newcost = cost + stepcost
+          if nextstate in costs:
+              oldcost = costs[nextstate]
+          else:
+              oldcost = float('inf')
+          if newcost < oldcost:
+              costs[nextstate] = newcost
+              counter = counter + 1
+              newpath = path + [action]
+              heapq.heappush(queue, (newcost, counter, nextstate, newpath))
+  return []
+     
